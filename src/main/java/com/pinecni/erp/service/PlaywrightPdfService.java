2111506ae83bb895563da064d6ba71019e5bf776
an.yonghak <neoshaby@gmail.com>
2026-02-13 16:04:34 +0900

    fix: PlaywrightPdfService 컴파일 오류 수정 (Missing Import)
    - java.io.File import 누락 수정

--- src/main/java/com/pinecni/erp/service/PlaywrightPdfService.java
+++ src/main/java/com/pinecni/erp/service/PlaywrightPdfService.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
